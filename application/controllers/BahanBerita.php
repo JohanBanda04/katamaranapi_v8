d797167a9519b0a01bf5c2cafc61ddc6a282184b
JohanBanda04 <banda04.johan@gmail.com>
2023-06-13 16:11:36 +0800

    katamaranapi_v8

---
+++ application/controllers/BahanBerita.php
@@ -0,0 +1,146 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class BahanBerita extends CI_Controller
+{
+    var $table_name = 'bahan_berita';
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function index()
+    {
+        $method = $_SERVER['REQUEST_METHOD'];
+        if ($method != 'GET') {
+            json_output(400, array('status' => 400, 'message' => 'Bad request.'));
+        } else {
+            $check_auth_client = $this->MyModel->check_auth_client();
+            if ($check_auth_client == true) {
+                $response = $this->MyModel->auth();
+                if ($response['status'] == 200) {
+                    $resp = $this->MyModel->get_all_rows_table($this->table_name);//GET SEMUA ROW TABEL
+                    json_output($response['status'], $resp);
+                }
+            }
+        }
+    }
+
+    public function detail($id) //parameter didapet dari url
+    {
+        $method = $_SERVER['REQUEST_METHOD'];
+        if ($method != 'GET' || $this->uri->segment(3) == '' || is_numeric($this->uri->segment(3)) == FALSE) {
+            json_output(400, array('status' => 400, 'message' => 'Bad request.'));
+        } else {
+            $check_auth_client = $this->MyModel->check_auth_client();
+            if ($check_auth_client == true) {
+                $response = $this->MyModel->auth();
+                if ($response['status'] == 200) {
+                    $resp = $this->MyModel->get_row_detail($this->table_name, $id);
+                    json_output($response['status'], $resp);
+                }
+            }
+        }
+    }
+
+    public function bahanBeritaByTanggal($tanggal) //parameter didapet dari url
+    {
+        $method = $_SERVER['REQUEST_METHOD'];
+        if ($method != 'GET' || $this->uri->segment(3) == '') {
+            json_output(400, array('status' => 400, 'message' => 'Bad request.'));
+        } else {
+            $check_auth_client = $this->MyModel->check_auth_client();
+            if ($check_auth_client == true) {
+                $response = $this->MyModel->auth();
+                if ($response['status'] == 200) {
+                    $resp = $this->MyModel->bahanBeritaByTanggal($tanggal);
+                    json_output($response['status'], $resp);
+                }
+            }
+        }
+    }
+
+    public function create()
+    {
+        $method = $_SERVER['REQUEST_METHOD'];
+        if ($method != 'POST') {
+            json_output(400, array('status' => 400, 'message' => 'Bad request.'));
+        } else {
+            $check_auth_client = $this->MyModel->check_auth_client();
+            if ($check_auth_client == true) {
+                $response = $this->MyModel->auth();
+                $respStatus = $response['status'];
+                if ($response['status'] == 200) {
+                    $params = json_decode(file_get_contents('php://input'), TRUE);
+                    if (empty($params['berita_what'])) {//ISI NAMA PARAMETER INPUT POST NYA
+                        $respStatus = 400;
+                        $resp = array('status' => 400, 'message' => 'Input form masih salah, silahkan coba lagi');
+                    } else {
+                        $resp = $this->MyModel->insert_to_table($this->table_name, $params);
+                    }
+                    json_output($respStatus, $resp);
+                }
+            }
+        }
+    }
+
+    public function update($id)
+    {
+        $method = $_SERVER['REQUEST_METHOD'];
+        if ($method != 'PUT' || $this->uri->segment(3) == '' || is_numeric($this->uri->segment(3)) == FALSE) {
+            json_output(400, array('status' => 400, 'message' => 'Bad request.'));
+        } else {
+            $check_auth_client = $this->MyModel->check_auth_client();
+            if ($check_auth_client == true) {
+                $response = $this->MyModel->auth();
+                $respStatus = $response['status'];
+                if ($response['status'] == 200) {
+                    $params = json_decode(file_get_contents('php://input'), TRUE);
+                    $params['updated_at'] = date('Y-m-d H:i:s');
+                    $resp = $this->MyModel->update_data_table($this->table_name, $id, $params);
+                    json_output($respStatus, $resp);
+                }
+            }
+        }
+    }
+
+    public function deleteFile($id)
+    {
+        $method = $_SERVER['REQUEST_METHOD'];
+        if ($method != 'POST' || $this->uri->segment(3) == '' || is_numeric($this->uri->segment(3)) == FALSE) {
+            json_output(400, array('status' => 400, 'message' => 'Bad request.'));
+        } else {
+            $check_auth_client = $this->MyModel->check_auth_client();
+            if ($check_auth_client == true) {
+                $response = $this->MyModel->auth();
+                $respStatus = $response['status'];
+                if ($response['status'] == 200) {
+                    $params = json_decode(file_get_contents('php://input'), TRUE);
+                    $params['updated_at'] = date('Y-m-d H:i:s');
+
+                    $resp = $this->MyModel->update_data_table($this->table_name, $id, $params);
+
+                    json_output($respStatus, $resp);
+                }
+            }
+        }
+    }
+
+    public function delete($id)
+    {
+        $method = $_SERVER['REQUEST_METHOD'];
+        if ($method != 'DELETE' || $this->uri->segment(3) == '' || is_numeric($this->uri->segment(3)) == FALSE) {
+            json_output(400, array('status' => 400, 'message' => 'Bad request.'));
+        } else {
+            $check_auth_client = $this->MyModel->check_auth_client();
+            if ($check_auth_client == true) {
+                $response = $this->MyModel->auth();
+                if ($response['status'] == 200) {
+                    $resp = $this->MyModel->delete_data_table($this->table_name, $id); //DELETE SINGLE ROW
+                    json_output($response['status'], $resp);
+                }
+            }
+        }
+    }
+}
